239c33249eba64ee706db4c7c846e920e7d7fe50
Sikorski, Alex <ASIKORSK@travelers.com>
2023-05-23 14:14:42 +0100

    initialised java 11 gradle project

---
+++ src/main/java/net/travp/quotingengine/QuotingEngineApplication.java
@@ -0,0 +1,13 @@
+package net.travp.quotingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/travp/quotingengine/QuotingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package net.travp.quotingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
